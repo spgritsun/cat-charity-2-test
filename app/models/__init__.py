c2041f535b1bcd4ff78186f1d2eee032a402f835
Sergei Gritsun <gritsun.sp@gmail.ru>
2026-09-16 20:16:08 +0300

    ready to go 1

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
 from app.models.charity_project import CharityProject  # noqa: F401
 from app.models.donation import Donation  # noqa: F401
-from .user import User  # noqa: F401
+from app.models.user import User  # noqa: F401
