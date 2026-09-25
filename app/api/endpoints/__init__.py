fbf76814ac86f598a714e8ba7768aea8646aabbf
Sergei Gritsun <gritsun.sp@gmail.ru>
2026-09-25 16:55:37 +0300

    Correct app/api/routers.py

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -3,4 +3,4 @@ from app.api.endpoints.charity_project import (  # noqa: F401
 )
 from app.api.endpoints.donation import router as donation_router  # noqa: F401
 from app.api.endpoints.user import router as user_router  # noqa: F401
-from .yandex_api import router as yandex_router # noqa: F401
+from .yandex_api import router as yandex_router  # noqa: F401
--- app/api/routers.py
+++ app/api/routers.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import (charity_project_router,
-                               donation_router, user_router)
+from app.api.endpoints import (charity_project_router, donation_router,
+                               user_router, yandex_router)
 
 main_router = APIRouter()
 main_router.include_router(charity_project_router, prefix='/charity_project',
                            tags=['charity_projects'])
 main_router.include_router(donation_router, prefix='/donation',
                            tags=['donations'])
+main_router.include_router(yandex_router, prefix='/yandex',
+                           tags=['Yandex Disk'])
 main_router.include_router(user_router)
